3390b481a334c598cb003e2917a0028e13527fb2
Greg Marsh <gregorytm@users.noreply.github.com>
2021-04-13 17:37:39 -0700

    special issues

--- timeWorld.js
+++ timeWorld.js
@@ -1,51 +1,65 @@
+const rawDataMinutes =
+  "o'clock one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen ninteen twenty twenty-one twenty-two twenty-three twenty-four twenty-five twenty-six twenty-seven twenty-eight twenty-nine thirty thirty-one thirty-two thirty-three thirty-four thirty-five thirty-six thirty-seven thirty-eight thirty-nine fourty fourty-one fourty-two fourty-three fourty-four fourty-five fourty-six fourty-seven fourty=eight fourty-nine fifty fifty-one fifty-two fifty-three fifty-four fifty-five fifty-six fifty-seven fifty-eight fifty-nine";
+
+const rawDataHours =
+  "twelve one two three four five six seven eight nine ten eleven twelve one two three four five six seven eight nine ten elven";
+
 function worldTime(args) {
   const hours = getHours(args);
   const min = getMin(args);
+  const special = noonAndMidnight(hours, min);
+  if (special) {
+    return special;
+  }
+  const amPm = amPmCheck(hours);
+  const checkListHours = rawToList(rawDataHours);
+  const checkListMinutes = rawToList(rawDataMinutes);
+  const hoursMatch = findMatch(hours, checkListHours);
+  const minutesMatch = findMatch(min, checkListMinutes);
+  const final = join(hoursMatch, minutesMatch, amPm);
+  return final;
 }
 
 function getHours(args) {
   const hours = args.slice(0, 2);
-  console.log(hours);
   return hours;
 }
 
 function getMin(args) {
   const min = args.slice(3);
-  console.log(min);
   return min;
 }
 
-function hourCheck(args) {
-  const checkList = {
-    00: "twelve",
-    01: "one",
-    02: "two",
-    03: "three",
-    04: "four",
-    05: "five",
-    06: "six",
-    07: "seven",
-    08: "eight",
-    09: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "one",
-    14: "two",
-    15: "three",
-    16: "four",
-    17: "five",
-    18: "six",
-    19: "seven",
-    20: "eight",
-    21: "nine",
-    22: "ten",
-    23: "eleven",
-  };
+function amPmCheck(hours) {
+  if (Number(hours) <= 11) {
+    return "am";
+  } else {
+    return "pm";
+  }
 }
 
-function minCheck(args) {
-  const checkList = {
-    00: "o'clock",
-  };
+function findMatch(arg, list) {
+  return list[arg];
+}
+
+function rawToList(rawData) {
+  const checkList = rawData.split(" ");
+  const checkDict = {};
+  for (const [i, v] of checkList.entries()) {
+    checkDict[i] = v;
+  }
+  return checkDict;
+}
+
+function noonAndMidnight(hours, min) {
+  if (hours === "0" && min === "0") {
+    return "midnight";
+  }
+  if (hours === "12" && min === "0") {
+    return "noon";
+  }
+}
+
+function join(hoursMatch, minutesMatch, amPm) {
+  return `${hoursMatch} ${minutesMatch} ${amPm}`;
 }
